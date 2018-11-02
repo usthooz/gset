7f785ff97f20b6662f75b66ac0768eab5c14b685
swxctx <swxctxone@yeah.net>
2018-11-02 15:49:00 +0800

    fix code

--- gset_unsafe_test.go
+++ gset_unsafe_test.go
@@ -3,14 +3,14 @@ package gset
 import "testing"
 
 func Test_GsetUnsafe_Add(t *testing.T) {
-	sa := New()
+	sa := New(ThreadUnSafe)
 	sa.Add("1")
 	sa.Add("2")
 	t.Logf("len: %d", sa.Len())
 }
 
 func Test_GsetUnsafe_Remove(t *testing.T) {
-	sa := New()
+	sa := New(ThreadUnSafe)
 	sa.Add("1")
 	sa.Add("2")
 	sa.Remove("1")
@@ -18,16 +18,16 @@ func Test_GsetUnsafe_Remove(t *testing.T) {
 }
 
 func Test_GsetUnsafe_IsEmpty(t *testing.T) {
-	sa := New()
+	sa := New(ThreadUnSafe)
 	sa.Add("1")
 	t.Log("is empty:", sa.IsEmpty())
 }
 
 func Test_GsetUnsafe_Merge(t *testing.T) {
-	sa := New()
+	sa := New(ThreadUnSafe)
 	sa.Add("1")
 	sa.Add("2")
-	sa1 := New()
+	sa1 := New(ThreadUnSafe)
 	sa1.Add("1")
 	sa1.Add("3")
 	sa.Merge(sa1)
@@ -35,7 +35,7 @@ func Test_GsetUnsafe_Merge(t *testing.T) {
 }
 
 func Test_GsetUnsafe_Clear(t *testing.T) {
-	sa := New()
+	sa := New(ThreadUnSafe)
 	sa.Add("1")
 	sa.Add("2")
 	t.Logf("sa len: %d", sa.Len())
