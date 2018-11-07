734b445ec659f94ba161ea955aebd409f8f6a94f
swxctx <swxctxone@yeah.net>
2018-11-07 10:43:35 +0800

    add slice convert method

--- gset.go
+++ gset.go
@@ -83,3 +83,94 @@ func Intersection(set1, set2 Gset, sets ...Gset) Gset {
 	})
 	return result
 }
+
+// StringSlice
+func StringSlice(s Gset) []string {
+	slice := make([]string, 0)
+	for _, item := range s.List() {
+		v, ok := item.(string)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// IntSlice
+func IntSlice(s Gset) []int {
+	slice := make([]int, 0)
+	for _, item := range s.List() {
+		v, ok := item.(int)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// Int32Slice
+func Int32Slice(s Gset) []int32 {
+	slice := make([]int32, 0)
+	for _, item := range s.List() {
+		v, ok := item.(int32)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// Int64Slice
+func Int64Slice(s Gset) []int64 {
+	slice := make([]int64, 0)
+	for _, item := range s.List() {
+		v, ok := item.(int64)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// Float32Slice
+func Float32Slice(s Gset) []float32 {
+	slice := make([]float32, 0)
+	for _, item := range s.List() {
+		v, ok := item.(float32)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// Float64Slice
+func Float64Slice(s Gset) []float64 {
+	slice := make([]float64, 0)
+	for _, item := range s.List() {
+		v, ok := item.(float64)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+// BoolSlice
+func BoolSlice(s Gset) []bool {
+	slice := make([]bool, 0)
+	for _, item := range s.List() {
+		v, ok := item.(bool)
+		if !ok {
+			continue
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
